cb1f97e4d70728ce2701608a67a11c9918c74614
Walker Le <walker.le88@gmail.com>
2023-06-13 21:10:58 -0600

    javascript complete

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,5 +1,27 @@
 function recursiveSearch(arr, target) {
   // type your code here
+  // SOLUTION:
+  if(arr.length === 0) {
+    return false;
+  }
+
+  if(arr[0] === target) {
+    return true;
+  }
+
+  arr.shift()
+  return recursiveSearch(arr, target)
+  // // return recursiveSearch(arr.slice(1), target)
+
+  // MY ATTEMPT:
+  // if(arr[0] === target && arr.length > 0) {
+  //   return true;
+  // } else if(arr[0] !== target && arr.length > 0) {
+  //   arr.shift();
+  //   return recursiveSearch(arr, target);
+  // } else {
+  //   return false;
+  // }
 }
 
 if (require.main === module) {
@@ -17,3 +39,8 @@ module.exports = recursiveSearch;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// Define a base case: if arr[index = 0] === target => return true
+// Loop to next array index => arr[index + 1]
+
+// .shift? => Destructively removes first element of array
